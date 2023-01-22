122ff421601ce4987eff26bdef6bc3fe0bf56968
agoscini <01169825@pw.edu.pl>
2023-01-22 20:45:01 +0100

    Coś jest w AppController ale nie wiem czy to jest dobrze ogólnie mało wiem

--- src/main/java/bdbt_bada_project/SpringApplication/AppController.java
+++ src/main/java/bdbt_bada_project/SpringApplication/AppController.java
@@ -65,13 +65,80 @@ public class AppController implements WebMvcConfigurer {
     }
     //user--------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
+
     //amdin-------------------------------------------------------------------------------------------------------------------------------------------------------------------
+    //adoptujacy
+    @RequestMapping("/adoptujacy")
+    public String adoptujacyView(Model model){
+        List<Adoptujacy> listAdoptujacy = daoAd.listAdmin();
+        model.addAttribute("listAdoptujacy", listAdoptujacy);
+        return "admin/adoptujacy";
+    }
+    @RequestMapping("/edit_adoptujacy/{nr_adoptujacego}")
+    public ModelAndView showEditFormAdoptujacy(@PathVariable(name = "nr_adoptujacego") int nr_adoptujacego){
+        ModelAndView mav = new ModelAndView("admin/edit_pracownicy");
+        Adoptujacy adoptujacy = daoAd.get(nr_adoptujacego);
+        mav.addObject("adoptujacy", adoptujacy);
+
+        return mav;
+    }
+    @RequestMapping(value="/updateAdoptujacy", method=RequestMethod.POST)
+    public String updateAdoptujacy(@ModelAttribute("adoptujacy") Adoptujacy adoptujacy){
+        daoAd.update(adoptujacy);
+
+        return "redirect:/adoptujacy";
+    }
+
+    //adresy
+    @RequestMapping("/adresy")
+    public String adresyView(Model model){
+        List<Adresy> listAdresy = daoAdr.list();
+        model.addAttribute("listAdresy", listAdresy);
+        return "admin/adresy";
+    }
+    @RequestMapping("/edit_adresy/{nr_adresu}")
+    public ModelAndView showEditFormAdresy(@PathVariable(name = "nr_adresu") int nr_adresu){
+        ModelAndView mav = new ModelAndView("admin/edit_pracownicy");
+        Adresy adresy = daoAdr.get(nr_adresu);
+        mav.addObject("adresy", adresy);
+
+        return mav;
+    }
+    @RequestMapping(value="/updateAdresy", method=RequestMethod.POST)
+    public String updateAdresy(@ModelAttribute("adresy") Adresy adresy){
+        daoAdr.update(adresy);
+
+        return "redirect:/adresy";
+    }
+
+    //klatki
+    @RequestMapping("/klatki")
+    public String klatkiView(Model model){
+        List<Klatki> listStudia = daoKl.list();
+        model.addAttribute("listStudia", listStudia);
+        return "admin/klatki";
+    }
+    @RequestMapping("/edit_klatki/{nr_klatki}")
+    public ModelAndView showEditFormStudia(@PathVariable(name = "nr_klatki") int nr_klatki){
+        ModelAndView mav = new ModelAndView("admin/edit_klatki");
+        Klatki klatki = daoKl.get(nr_klatki);
+        mav.addObject("klatki", klatki);
+
+        return mav;
+    }
+    @RequestMapping(value="/updateKlatki", method=RequestMethod.POST)
+    public String updateStudia(@ModelAttribute("klatki") Klatki klatki){
+        daoKl.update(klatki);
+
+        return "redirect:/klatki";
+    }
+
     //pracownicy
-    @RequestMapping("/pracownicy_zarzad")
+    @RequestMapping("/pracownicy")
     public String pracownicyView(Model model){
         List<Pracownicy> listPracownicy = daoPr.list();
         model.addAttribute("listPracownicy", listPracownicy);
-        return "admin/pracownicy_zarzad";
+        return "admin/pracownicy";
     }
     @RequestMapping("/edit_pracownicy/{nr_pracownika}")
     public ModelAndView showEditFormPracownicy(@PathVariable(name = "nr_pracownika") int nr_pracownika){
@@ -85,9 +152,51 @@ public class AppController implements WebMvcConfigurer {
     public String updatePracownicy(@ModelAttribute("pracownicy") Pracownicy pracownicy){
         daoPr.update(pracownicy);
 
-        return "redirect:/pracownicy_zarzad";
+        return "redirect:/pracownicy";
     }
 
+    //zwierzeta
+    @RequestMapping("/zwierzeta")
+    public String zwierzetaView(Model model){
+        List<Zwierzeta> listZwierzeta = daoZw.list();
+        model.addAttribute("listZwierzeta", listZwierzeta);
+        return "admin/zwierzeta";
+    }
+   // @RequestMapping("/new_zwierzeta")
+   // public String showNewFormZwierzeta(Model model) {
+   //     Zwierzeta zwierzeta = new Zwierzeta();
+   //     model.addAttribute("zwierzeta", zwierzeta);
+   //     return "admin/new_zwierzeta";
+   // }
+    // tutaj coś nie działa
+    // ogólnie więcej nie mam pomysłu jak zrobić
+
+    @RequestMapping(value="/saveZwierzeta", method=RequestMethod.POST)
+    public String saveZwierzeta(@ModelAttribute("zwierzeta") Zwierzeta zwierzeta){
+        daoZw.save(zwierzeta);
+        return "redirect:/zwierzeta";
+    }
+    @RequestMapping("/edit_zwierzeta/{nr_zwierzecia}")
+    public ModelAndView showEditFormZwierzeta(@PathVariable(name = "nr_zwierzecia") int nr_zwierzecia){
+        ModelAndView mav = new ModelAndView("admin/edit_sprzety");
+        Zwierzeta zwierzeta = daoZw.get(nr_zwierzecia);
+        mav.addObject("zwierzeta", zwierzeta);
+
+        return mav;
+    }
+    @RequestMapping(value="/updateZwierzeta", method=RequestMethod.POST)
+    public String updateZwierzeta(@ModelAttribute("zwierzeta") Zwierzeta zwierzeta){
+        daoZw.update(zwierzeta);
+
+        return "redirect:/zwierzeta";
+    }
+
+    @RequestMapping("/delete/{nr_zwierzecia}")
+    public String deleteZwierzeta(@PathVariable(name = "nr_zwierzecia") int nr_zwierzecia){
+        daoZw.delete(nr_zwierzecia);
+
+        return "redirect:/zwierzeta";
+    }
 
 
 }
